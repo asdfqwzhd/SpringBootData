b503bd009cc862bd2b5aa4eb19522fa2c89ba72b
zhd <asdfqwzhd@vip.qq.com>
2022-03-26 00:50:39 +0800

    mybatis mapper更新

--- src/main/java/com/springbootdata/test/postgres/controller/UserController.java
+++ src/main/java/com/springbootdata/test/postgres/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.springbootdata.test.postgres.controller;
 
+import com.springbootdata.test.common.valueobject.SystemItems;
+import com.springbootdata.test.elasticsearch.document.UserDocument;
+import com.springbootdata.test.elasticsearch.repository.UserDocumentRepository;
 import com.springbootdata.test.postgres.entity.UserEntity;
 import com.springbootdata.test.postgres.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/postgres/user")
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserMapper userMapper;
 
+    private final UserDocumentRepository userDocumentRepository;
 
     /**
      * 用户取得
@@ -26,6 +32,44 @@ public class UserController {
     @ResponseBody
     public UserEntity getUserByUserId(@PathVariable(value = "userId") String userId) {
         UserEntity userEntity = this.userMapper.sel(userId);
+//        Company company = new Company("123","1243");
         return userEntity;
+    }
+
+    /**
+     * 用户登录更新-单用户
+     * URL例：POST：http://localhost:8080/elasticsearch/user/single
+     * RequestBody：user-single-insert.json
+     * @param user 用户信息
+     * @return 成功
+     */
+    @PostMapping("/single")
+    @ResponseBody
+    public String saveSingleUser(@RequestBody UserEntity user){
+        user.setSystemItems(new SystemItems(LocalDateTime.now(),LocalDateTime.now()));
+        this.userMapper.save(user);
+        UserDocument userDocument = new UserDocument(user.getUser().getUserId(), user.getUser(),user.getCompany(),user.getDepartment(),user.getValidPeriod(),user.getSystemItems());
+        this.userDocumentRepository.save(userDocument);
+        return "SUCCESS";
+    }
+
+    /**
+     * 用户登录更新-多用户
+     * URL例：POST：http://localhost:8080/elasticsearch/user/multiple
+     * RequestBody：user-multiple-insert.json
+     * @param users 用户信息列表
+     * @return 成功
+     */
+    @PostMapping("/multiple")
+    @ResponseBody
+    public String saveMultipleUser(@RequestBody List<UserEntity> users){
+        List<UserDocument> userDocuments = new ArrayList<>();
+        users.forEach(user->{
+            user.setSystemItems(new SystemItems(LocalDateTime.now(),LocalDateTime.now()));
+            this.userMapper.save(user);
+            userDocuments.add(new UserDocument(user.getUser().getUserId(), user.getUser(),user.getCompany(),user.getDepartment(),user.getValidPeriod(),user.getSystemItems()));
+        });
+        this.userDocumentRepository.saveAll(userDocuments);
+        return "SUCCESS";
     }
 }
--- src/main/java/com/springbootdata/test/postgres/mapper/UserMapper.java
+++ src/main/java/com/springbootdata/test/postgres/mapper/UserMapper.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMapper {
     UserEntity sel(String id);
+
+    void insert(UserEntity user);
+
+    void save(UserEntity user);
 }
