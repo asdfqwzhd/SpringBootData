ff791b7894d68941ca1c37986d9ee20ae0d8f07f
zhd <asdfqwzhd@vip.qq.com>
2022-03-26 00:43:38 +0800

    ddl更新

--- src/main/resources/db/migration/R__initial_table.sql
+++ src/main/resources/db/migration/R__initial_table.sql
@@ -2,5 +2,14 @@ DROP TABLE IF EXISTS "user";
 CREATE TABLE "user"(
   user_id varchar(6) NOT NULL,
   user_name varchar(32) NOT NULL,
+  company_code varchar(2) NOT NULL,
+  company_name varchar(32) NOT NULL,
+  department_company_code varchar(2),
+  department_no varchar(4),
+  department_name varchar(32),
+  valid_start_day date NOT NULL,
+  valid_end_day date NOT NULL,
+  insert_date_time timestamp NOT NULL,
+  update_date_time timestamp NOT NULL,
   PRIMARY KEY (user_id)
 );
